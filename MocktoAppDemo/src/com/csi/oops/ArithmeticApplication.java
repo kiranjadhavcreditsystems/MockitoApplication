c09bd3dc8dca31d317c2df38a98fb2c6e105e04f
admin <admin@192.168.1.208>
2021-01-15 08:08:18 +0530

    Addedd Addition of 2 number logic

---
+++ MocktoAppDemo/src/com/csi/oops/ArithmeticApplication.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.csi.oops;
+
+/**
+ * @author admin
+ *
+ */
+public class ArithmeticApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n1=20, n2=10, result=0;
+		result=n1+n2;
+		System.out.println(result);
+	}
+
+}
